05c2ef18b789e51cb8021d53568924df580cfa8d
tavindev <gvwchevrand@gmail.com>
2024-12-06 18:19:19 +0000

    refactor: create register screen method

--- worldedit-fabric/src/main/java/com/sk89q/worldedit/fabric/FabricKeyHandler.java
+++ worldedit-fabric/src/main/java/com/sk89q/worldedit/fabric/FabricKeyHandler.java
@@ -6,71 +6,33 @@ import com.sk89q.worldedit.fabric.gui.screens.WorldEditQuickMenu;
 import com.sk89q.worldedit.fabric.gui.screens.WorldEditRebrushCommandScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.KeyMapping;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import org.lwjgl.glfw.GLFW;
 
 public class FabricKeyHandler implements ClientModInitializer {
 
-    private static KeyMapping openCommandListKey;
-    private static KeyMapping openQuickMenuKey;
-    private static KeyMapping openBrushToolKey;
-    private static KeyMapping openRebrushToolKey;
 
     @Override
     public void onInitializeClient() {
-        openCommandListKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
-                "key.fabricworldedit.open_command_list",
-                GLFW.GLFW_KEY_H,
-                "category.fabricworldedit.general"
-        ));
-
-        openQuickMenuKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
-                "key.fabricworldedit.open_quick_menu",
-                GLFW.GLFW_KEY_G,
-                "category.fabricworldedit.general"
-        ));
-
-        openBrushToolKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
-                "key.fabricworldedit.open_brush_tool",
-                GLFW.GLFW_KEY_B,
-                "category.fabricworldedit.general"
-        ));
-
-        openRebrushToolKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
-                "key.fabricworldedit.open_rebrush_tool",
-                GLFW.GLFW_KEY_R,
-                "category.fabricworldedit.general"
-        ));
-
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (openCommandListKey.isDown()) {
-                toggleScreen(client, new WorldEditCommandScreen());
-            }
-            if (openQuickMenuKey.isDown()) {
-                toggleScreen(client, new WorldEditQuickMenu());
-            }
-            if (openBrushToolKey.isDown()) {
-                toggleScreen(client, new WorldEditBrushCommandScreen());
-            }
-            if (openRebrushToolKey.isDown()) {
-                toggleScreen(client, new WorldEditRebrushCommandScreen());
-            }
-        });
+        registerScreen(new WorldEditCommandScreen(), "key.fabricworldedit.open_command_list", GLFW.GLFW_KEY_H, "category.fabricworldedit.general");
+        registerScreen(new WorldEditQuickMenu(), "key.fabricworldedit.open_quick_menu", GLFW.GLFW_KEY_G, "category.fabricworldedit.general");
+        registerScreen(new WorldEditBrushCommandScreen(), "key.fabricworldedit.open_brush_tool", GLFW.GLFW_KEY_B, "category.fabricworldedit.general");
+        registerScreen(new WorldEditRebrushCommandScreen(), "key.fabricworldedit.open_rebrush_tool", GLFW.GLFW_KEY_R, "category.fabricworldedit.general");
     }
 
-    /**
-     * Toggles a screen: opens it if it's not open, closes it if it's the current screen.
-     */
-    private void toggleScreen(Minecraft client, Screen screen) {
-        if (client.player != null) {
+    public void registerScreen(Screen screen, String string1, int i, String string2) {
+        KeyMapping keyMapping = new KeyMapping(string1, i, string2);
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (!keyMapping.isDown() || client.player == null) return;
+
             if (client.screen != null && client.screen.getClass().equals(screen.getClass())) {
                 client.setScreen(null);
             } else {
                 client.setScreen(screen);
             }
-        }
+        });
     }
+
 }
