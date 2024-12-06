fe7404bc83f01895d29ff07cd2a5413e85ab3fd5
amorim-dm <pedrowamorim@gmail.com>
2024-12-06 22:02:56 +0000

    Rename Class

--- worldedit-fabric/src/main/java/com/sk89q/worldedit/fabric/gui/screens/WorldEditBrushCommandScreen.java
+++ worldedit-fabric/src/main/java/com/sk89q/worldedit/fabric/gui/screens/WorldEditBrushCommandScreen.java
@@ -20,7 +20,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class WorldEditBrushEdit extends Screen {
+public class WorldEditBrushCommandScreen extends Screen {
     private static final Component MODE_LABEL = Component.translatable("Create Brush Tool");
     private static final Component DEFAULT_OPTION = Component.translatable("Select Brush");
     private static final Component DEFAULT_ITEM_OPTION = Component.translatable("Select Item");
@@ -56,7 +56,7 @@ public class WorldEditBrushEdit extends Screen {
 
     private String brushTypeSelected = "cuboid";
 
-    public WorldEditBrushEdit() {
+    public WorldEditBrushCommandScreen() {
         super(GameNarrator.NO_TITLE);
     }
 
